55a6d36527b4c39fb4a326a19f786aec3e147dda
Ebrahim Suhail Al-Yousefi <ebra.alyousefi@gmail.com>
2025-07-22 12:06:28 -0700

    Revert "Initial backend code with Gemini API chat endpoint"

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class PromptRequest(BaseModel):
-    prompt: str # json body request  -> {"prompt":"..."}
-
-class PromptResponse(BaseModel):
-    response: str # retuurn json body ->{"res":"..."}
